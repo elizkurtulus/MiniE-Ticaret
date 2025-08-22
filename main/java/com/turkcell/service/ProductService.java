1737cc6992ca1fc71bb3d155268648cf272a7e14
yasarerenn <yasar.eren33@gmail.com>
2025-08-22 18:48:39 +0300

    - açıklamalar eklendi - import java.util.List; satırı kaldırıldı. - çıktılar düzenlendi.

--- main/java/com/turkcell/service/ProductService.java
+++ main/java/com/turkcell/service/ProductService.java
@@ -1,30 +1,37 @@
-package com.turkcell.service;
+package com.turkcell.service; // Bu sınıfın service paketinde olduğunu belirtiyoruz.
 
+// Gerekli sınıfları import ediyoruz.
 import com.turkcell.entity.Product;
 import com.turkcell.repository.ProductRepository;
 
-import java.util.List;
 
 public class ProductService {
-    ProductRepository productRepository;
+    ProductRepository productRepository; // ProductRepository nesnesini tutan field - Dependency Injection için
 
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+
+    public ProductService(ProductRepository productRepository) // Constructor - ProductRepository'yi dependency injection ile alıyoruz.
+    {
+        this.productRepository = productRepository; // Gelen repository'yi field'a atıyoruz.
     }
 
+    // Yeni ürün ekleme metodu tanımlıyoruz.
     public void addProduct(Product product) {
         productRepository.add(product);
     }
 
+    // Tüm ürünleri listeleme metodu tanımlıyoruz.
     public void getProducts() {
+    System.out.println("----------------------------------------------------------------------------------------------------");
+    System.out.println("                                Ürünler Listeleniyor...");
         for(Product product : productRepository.getAll()) {
-            System.out.println("Product Id: " + product.getId() + " Product Name: " + product.getName()
-                    + " Price: " + product.getPrice() + " Stock: " + product.getStock());
+            System.out.println("Ürün Id: " + product.getId() + " | Ürün Adı: " + product.getName()
+                    + " | Ürün Fiyatı: " + product.getPrice() + " | Stok: " + product.getStock());
         }
+    System.out.println("----------------------------------------------------------------------------------------------------");
     }
 
-    public int checkStock(int id) {
-        Product product = productRepository.findById(id);
-        return product.getStock();
+    public int checkStock(int id) { //Id'e göre ürün stok kontrolü metodu tanımlıyoruz.
+        Product product = productRepository.findById(id); // Ürünü Id'e göre buluyoruz.
+        return product.getStock(); // Ürünün stok miktarını dönüyoruz.
     }
 }
